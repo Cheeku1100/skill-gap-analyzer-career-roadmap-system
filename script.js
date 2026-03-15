c53abd25ce3ff0ec17aa438fcc72535c45c4838c
Cheeku1100 <89233504+Cheeku1100@users.noreply.github.com>
2026-03-15 20:27:46 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function analyzeSkills() {
+    let skills = document.getElementById("skills").value;
+
+    if (skills === "") {
+        alert("Please enter your skills");
+    } else {
+        alert("Skill analysis feature will be implemented soon.");
+    }
+}
